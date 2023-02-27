eeb7de22eabccec918114b0589750558c860c201
Fzzy <fzzysoda@gmail.com>
2023-02-27 15:05:03 -0800

    sjdoaifjsa

--- extension.js
+++ extension.js
@@ -2,27 +2,27 @@ const fs = require('fs')
 
 var state = {}
 
-if (fs.existsSync('./state.json')) {
-  state = JSON.parse(fs.readFileSync("./state.json"))
+if (fs.existsSync(__dirname + '/state.json')) {
+  state = JSON.parse(fs.readFileSync(__dirname + "/state.json"))
 }
 
 module.exports = nodecg => {
   function sendDataUpdate(ignoreId) {
     state.socketId = ignoreId
-    nodecg.sendMessage('OnevsOneUpdate', state)
+    nodecg.sendMessage('StateUpdate', state)
     delete state.socketId
   }
-  nodecg.listenFor('OnevsOneStateRequest', (id) => {
+  nodecg.listenFor('StateRequest', (id) => {
     sendDataUpdate(id)
   });
-  nodecg.listenFor('UpdateOnevsOneData', (value) => {
+  nodecg.listenFor('UpdateStateData', (value) => {
     for (key in value) {
       state[key] = value[key]
     }
     sendDataUpdate(state.socketId)
     delete state.socketId
     const jsonString = JSON.stringify(state)
-    fs.writeFile('./OnevsOneState.json', jsonString, err => {
+    fs.writeFile(__dirname + '/state.json', jsonString, err => {
       if (err) {
         console.log('Error writing file', err)
       } else {
